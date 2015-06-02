e3b179441f34de08b35be4186527ad0e37a71946
git@daemon.de <git@daemon.de>
2015-06-02 09:50:44 +0200

    bump

--- NOTEDB.pm
+++ NOTEDB.pm
@@ -10,7 +10,7 @@ package NOTEDB;
 use Exporter ();
 use vars qw(@ISA @EXPORT $crypt_supported);
 
-$NOTEDB::VERSION = "1.42";
+$NOTEDB::VERSION = "1.43";
 
 BEGIN {
     # make sure, it works, otherwise encryption
